ca852dfaf40f3b182c49c0655d908eb811bdfb33
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2021-07-07 17:15:12 -0400

    Tile burning in progress
    Game doesn't work but I have a headache so I'll do this in a bit

--- Player.gd
+++ Player.gd
@@ -5,28 +5,29 @@ onready var iPos = (global_position)
 
 """
 TODO:
-	- Fix bump function
-	- Find a way to make walls work. Try to avoid obstacles like the tutorial and instead do a ray cast
-	or something to bump like in the original
+	- Implement tile burning
+	- Make burning work by decreasing the tile value every time the player moves.
 	- Make tweening/animation work the way you want it (polish)
 """
 
 func _process(_delta): #update
 	
-	global_position = lerp(global_position, iPos, _delta * 20)
+	global_position = lerp(global_position, iPos, _delta * 20) # movement lerp
 	
 	if Input.is_action_just_pressed("down"):
-		check_then_move(iPos.x, iPos.y + 64)
+		check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y + 64)
 	if Input.is_action_just_pressed("up"):
-		check_then_move(iPos.x, iPos.y - 64)
+		check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y - 64)
 	if Input.is_action_just_pressed("right"):
-		check_then_move(iPos.x + 64, iPos.y)
+		check_then_move(Grid.world_to_map(iPos), iPos.x + 64, iPos.y)
 	if Input.is_action_just_pressed("left"):
-		check_then_move(iPos.x - 64, iPos.y)
+		check_then_move(Grid.world_to_map(iPos), iPos.x - 64, iPos.y)
 
-func check_then_move(cx, cy):
-	var gridPos = Grid.world_to_map(Vector2(cx, cy))
-	print("gridPos: ", gridPos)
-	print("Tilemap value: ", Grid.get_cellv(gridPos))
-	if Grid.get_cellv(gridPos) != -1:
+func check_then_move(gridPos, cx, cy):
+	var newGridPos = Grid.world_to_map(Vector2(cx, cy))
+	print("newGridPos: ", newGridPos)
+	print("old GridPos: ", gridPos)
+	print("Tilemap value: ", Grid.get_cellv(newGridPos))
+	if Grid.get_cellv(newGridPos) != -1: # if tile isn't empty, commit move
 		iPos = Vector2(cx, cy)
+		Grid.set_cell(gridPos.x, gridPos.y, 13)
---
+++ TileMap.gd
@@ -0,0 +1,17 @@
+extends TileMap
+
+func _process(delta):
+	if get_used_cells_by_id(13): set_cell(12)
+#			13: # 6
+#				Grid.set_cell(12)
+#			12: # 5
+#				Grid.set_cell(11)
+#			11: # 4
+#				Grid.set_cell(10)
+#			10: # 3
+#				Grid.set_cell(9)
+#			9: # 2
+#				Grid.set_cell(8)
+#			8: # 1
+#				Grid.set_cell(7)
+
