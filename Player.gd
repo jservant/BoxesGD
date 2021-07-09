0fcd3c2a6a91a45a4a8b5e8cd6341caa860fac3a
Jaden S <jbroxnari@protonmail.com>
2021-07-09 18:10:30 -0400

    Tile burning works + rough player death

--- Player.gd
+++ Player.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var Grid = get_parent() # gets parent Tilemap
 onready var iPos = (global_position)
+var touchedTiles = []
+var ded = false
 
 """
 TODO:
@@ -24,10 +26,39 @@ func _process(_delta): #update
 		check_then_move(Grid.world_to_map(iPos), iPos.x - 64, iPos.y)
 
 func check_then_move(gridPos, cx, cy):
+	if (ded): return
 	var newGridPos = Grid.world_to_map(Vector2(cx, cy))
-	print("newGridPos: ", newGridPos)
-	print("old GridPos: ", gridPos)
-	print("Tilemap value: ", Grid.get_cellv(newGridPos))
+	# print("newGridPos: ", newGridPos)
+	# print("old GridPos: ", gridPos)
+	# print("Tilemap value: ", Grid.get_cellv(newGridPos))
 	if Grid.get_cellv(newGridPos) != -1: # if tile isn't empty, commit move
-		iPos = Vector2(cx, cy)
-		Grid.set_cell(gridPos.x, gridPos.y, 13)
+		if Grid.get_cellv(gridPos) == 0: # if tile is black square
+			touchedTiles.push_back(Vector3(gridPos.x, gridPos.y, 6)) # add tile that was touched to array
+			print("Length of touched tiles: ", touchedTiles.size()) 
+		iPos = Vector2(cx, cy) # move to new tile
+		for tile in range(0, touchedTiles.size()):
+			touchedTiles[tile].z = int(touchedTiles[tile].z) - 1
+			if (touchedTiles[tile].z == 0): 
+				Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), -1)
+				touchedTiles.erase(tile)
+			elif (touchedTiles[tile].z == 1):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 8)
+			elif (touchedTiles[tile].z == 2):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 9)
+			elif (touchedTiles[tile].z == 3):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 10)
+			elif (touchedTiles[tile].z == 4):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 11)
+			elif (touchedTiles[tile].z == 5):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 12)
+			elif (touchedTiles[tile].z == 6):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 13)
+		if (Grid.get_cellv(newGridPos) == -1):
+			ded = true
+			print("you ded lol")
+
+
+#			match int(tile.z):
+#				0:
+#					Grid.set_cellv(Vector2(tile.x, tile.y), -1)
+#					touchedTiles.erase(tile)
+#				1:  Grid.set_cellv(Vector2(tile.x, tile.y), 8)
+#				2:  Grid.set_cellv(Vector2(tile.x, tile.y), 9)
+#				3:  Grid.set_cellv(Vector2(tile.x, tile.y), 10)
+#				4:  Grid.set_cellv(Vector2(tile.x, tile.y), 11)
+#				5:  Grid.set_cellv(Vector2(tile.x, tile.y), 12)
+#				6:  Grid.set_cellv(Vector2(tile.x, tile.y), 13)
