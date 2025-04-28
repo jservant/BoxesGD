4daad9c1f5f7a7ccab7e0f95c4093f6f3fbe4c90
Jaden Servant <48299534+jservant@users.noreply.github.com>
2025-04-27 20:14:58 -0400

    Complete??

--- Player.gd
+++ Player.gd
@@ -2,30 +2,37 @@ extends Node2D
 
 onready var Grid = get_parent() # gets parent Tilemap
 onready var iPos = (global_position)
+onready var manager = get_tree().root.get_child(0)
+onready var sprite = $Sprite
+export var ladNum : int
+export var clearLimit = 2
+var active = true
 var allTiles = []
 var touchedTiles = []
 var ded = false
 
-"""
-TODO:
-	- Make tweening/animation work the way you want it (polish)
-"""
+signal new_lad(ladNum)
+
+func _ready():
+	if ladNum != manager.startingLad:
+		active = false
+		sprite.visible = false
 
 func _process(_delta): #update
-	
-	global_position = lerp(global_position, iPos, _delta * 20) # movement lerp
-	
-	if Input.is_action_just_pressed("restart"):
-		get_tree().reload_current_scene()
-	
-	if Input.is_action_just_pressed("down"):
-		check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y + 64)
-	if Input.is_action_just_pressed("up"):
-		check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y - 64)
-	if Input.is_action_just_pressed("right"):
-		check_then_move(Grid.world_to_map(iPos), iPos.x + 64, iPos.y)
-	if Input.is_action_just_pressed("left"):
-		check_then_move(Grid.world_to_map(iPos), iPos.x - 64, iPos.y)
+	if ladNum == manager.startingLad:
+		active = true
+		sprite.visible = true
+		
+		global_position = lerp(global_position, iPos, _delta * 20) # movement lerp
+		
+		if Input.is_action_just_pressed("down"):
+			check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y + 64)
+		if Input.is_action_just_pressed("up"):
+			check_then_move(Grid.world_to_map(iPos), iPos.x, iPos.y - 64)
+		if Input.is_action_just_pressed("right"):
+			check_then_move(Grid.world_to_map(iPos), iPos.x + 64, iPos.y)
+		if Input.is_action_just_pressed("left"):
+			check_then_move(Grid.world_to_map(iPos), iPos.x - 64, iPos.y)
 
 func check_then_move(gridPos, cx, cy):
 	if (ded): return
@@ -48,7 +55,11 @@ func check_then_move(gridPos, cx, cy):
 			elif (touchedTiles[tile].z == 6):  Grid.set_cellv(Vector2(touchedTiles[tile].x, touchedTiles[tile].y), 13)
 		if (Grid.get_cellv(newGridPos) == -1):
 			ded = true
-			$Sprite.visible = not $Sprite.visible
+			sprite.visible = false
 			print("you ded lol")
-		if (Grid.get_used_cells_by_id(0).size() < 2):
+		if (Grid.get_used_cells_by_id(0).size() < clearLimit): #win
+			active = false
+			sprite.visible = false
+			emit_signal("new_lad", ladNum)
 			print("you did it woo")
+			Grid.queue_free()
---
+++ manager.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+onready var start = $StartSound
+var hasStarted = false
+onready var startingLad = 1
+signal activateLad
+
+func _process(delta):
+	if Input.is_action_just_pressed("start") and hasStarted == false:
+		start.play()
+		hasStarted = true
+	if Input.is_action_just_pressed("restart") and hasStarted == true:
+		get_tree().reload_current_scene()
+		start.play()
+
+func _on_Lad_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+
+func _on_Lad2_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+
+func _on_Lad3_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+
+func _on_Lad4_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+
+func _on_Lad5_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+	#newLadObj.clearLimit = 3
+
+func _on_Lad6_new_lad(ladNum):
+	startingLad = ladNum + 1
+	var newLadObj = find_node("Lad"+str(startingLad))
+	newLadObj.active = true
+	#newLadObj.clearLimit = 3
