cc8d788a8f5be8b678d17573158e4ce6bf0d0d77
Jaden Servant <48299534+jservant@users.noreply.github.com>
2025-04-27 22:53:43 -0400

    most solid build so far?
    if only I could get it centered on my website

--- manager.gd
+++ manager.gd
@@ -2,16 +2,22 @@ extends Node2D
 
 onready var start = $StartSound
 var hasStarted = false
-onready var startingLad = 1
+onready var startingLad = 0
 signal activateLad
 
 func _process(delta):
 	if Input.is_action_just_pressed("start") and hasStarted == false:
 		start.play()
 		hasStarted = true
+		startingLad = 1
+		var newLadObj = find_node("Lad"+str(startingLad))
+		newLadObj.active = true
 	if Input.is_action_just_pressed("restart") and hasStarted == true:
 		get_tree().reload_current_scene()
 		start.play()
+		startingLad = 1
+		var newLadObj = find_node("Lad"+str(startingLad))
+		newLadObj.active = true
 
 func _on_Lad_new_lad(ladNum):
 	startingLad = ladNum + 1
